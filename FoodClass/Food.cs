c6a94b56634825053ef05cb50a98f12d6778d344
Vlad <Petrochenko>
2018-04-07 22:09:45 +0500

    FoodDelivery

---
+++ FoodClass/Food.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodClass
+{
+    public class Food
+    {
+        /// <summary>
+        /// Дата заполнения
+        /// </summary>
+        public DateTime Filled { get; set; }
+        /// <summary>
+        /// ФИО заказчика
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Адресс доставки
+        /// </summary>
+        public List<WayPoint> WayPoints { get; set; }
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public decimal Price { get; set; }
+        public Currency Currency { get; set; }
+        /// <summary>
+        /// Заказанные блюда
+        /// </summary>
+        public string Dishes { get; set; }
+        /// <summary>
+        /// Номер телефона заказчика
+        /// </summary>
+        public int PhoneNumber { get; set; }
+        /// <summary>
+        /// К какому времени доставить
+        /// </summary>
+        public DateTime DeliveryTime { get; set; }
+    }
+    public enum Currency
+    {
+        Rubles
+    }
+    public class WayPoint
+    {
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}", Address);
+        }
+
+        public WayPoint Clone()
+        {
+            return new WayPoint { Address = Address};
+        }
+    }
+}
---
+++ FoodClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FoodClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoodClass")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7263ab23-749f-40ad-91d1-7c7cf586d889")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
