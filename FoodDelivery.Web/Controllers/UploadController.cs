cb4a401ac1785ade2490261ad7029e4847be7ebb
Vlad <Petrochenko>
2018-05-30 22:15:22 +0500

    Web

---
+++ FoodDelivery.Web/Controllers/UploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FoodDelivery.Web.Models;
+
+namespace RemontWeb.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Print(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var dto = FoodClass.FoodDeliveryHelper.LoadFromStream(file.InputStream);
+
+                return View(dto);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FoodDelivery.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodDelivery.Web.Startup))]
+namespace FoodDelivery.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
